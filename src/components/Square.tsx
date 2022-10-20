aef34207cdf0e07c8cdd13b29552784f3f6bcbb8
Michal <michal.p.zarzycki@wp.pl>
2022-10-20 10:21:54 -0800

    Create Square component

---
+++ src/components/Square.tsx
@@ -0,0 +1,12 @@
+const Square = () => {
+  return (
+    <button
+      className="square"
+      style={{ backgroundColor: "black", color: 'white' }}
+    >
+     S
+    </button>
+  );
+};
+
+export default Square;
